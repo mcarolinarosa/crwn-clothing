b438868e8966aa9ab9e6b760b348979a11a1d032
Carolina Rosa <carolinarosa@novatronica.com>
2019-08-28 14:30:17 +0100

    add and remove items on checkout

--- src/redux/cart/cart.utils.jsx
+++ src/redux/cart/cart.utils.jsx
@@ -13,3 +13,27 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  //recebe a lista de todos os cart items , e o que se pretende remover
+  const existingCartItem = cartItems.find(
+    // existingCartItem é igual ao item que se pretende remover
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    // se a quantidade do item que se pretende remover for igual a 1
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id); // remove esse item
+  }
+
+  return cartItems.map((
+    cartItem // se for maior que 1
+  ) =>
+    cartItem.id === cartItemToRemove.id
+      ? {
+          ...cartItem,
+          quantity: cartItem.quantity - 1 //faz decrease da quantidade do item que se pretende diminuir
+        }
+      : cartItem
+  );
+};
